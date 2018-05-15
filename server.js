99e32237275ecdeb5f3db68721f7ac1623b42181
Glitch (chocolate-wizard) <none>
2018-05-15 04:53:21 +0000

    👷🐔 Checkpoint ./server.js:1047756/606

--- server.js
+++ server.js
@@ -32,12 +32,37 @@ app.get("/", function (request, response) {
 app.get("/https\://:url", function(req,resp){
   let regex= new RegExp("www");
   // match urlToShorten;
-  console.log(req.params.url.toString().match(/regex/));
+  //console.log(req.params.url.toString().match(/regex/));
+  let findfunc= function(err, done){
+  Url.findOne({url: req.params.url},function(err,data){
+    if (err) console.log(err);
+   if (data){
+     console.log(data);
+     done(null, data);
+     // resp.end(JSON.stringify(data));
+   }
+   // update record if data is null;
+   else{
+  Url.count(function(err,data){
+  if (err) console.log(err);
+    if(data){
+    let entry= new Url({
+    url: req.params.url,
+    shortened: data+1
+    });
+      entry.save();
+      done(null,data);
+    //resp.end(JSON.stringify(entry));
+    }
+  });
+  
+  }
  if(req.params.url.toString().match(regex)){
    console.log("passed regex");
  Url.findOne({url: req.params.url},function(err,data){
     if (err) console.log(err);
    if (data){
+     console.log(data);
      resp.end(JSON.stringify(data));
    }
    // update record if data is null;
